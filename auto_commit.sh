d1b8fbaabf7e65c95b8a5e4f2658800f4479ba2a
osariban <92905939+osariban@users.noreply.github.com>
2025-05-11 23:01:09 +0900

    auto_commit.sh を更新(bashでのバグ修正)

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 
 SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
-cd "$SCRIPT_PATH" || { echo "cd failed"; exit 1 }
+cd "$SCRIPT_PATH" || { echo "cd failed"; exit 1; }
 
 DATE=$(date +%Y-%m-%d)
 
